ca283dbb170f59877cb0c303867a0f4bae45ffa0
Ian Nery Bandeira <iannerybandeira@gmail.com>
2023-04-27 13:32:37 -0300

    Added Header and main content

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,58 +1,144 @@
-import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, useColorScheme, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { darkColors, lightColors } from "../../styles/colors";
+import { IconMap, IconNotif, IconOpt } from "../../assets/svg";
+
+const Header = () => (
+  <View
+    style={{
+      flexDirection: "row",
+      marginTop: 20,
+      paddingHorizontal: 20,
+      alignItems: "center",
+      justifyContent: "space-between",
+    }}
+  >
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 8,
+      }}
+    >
+      <IconMap />
+      <Text
+        style={{
+          fontSize: 18,
+          color: "#fff",
+          fontFamily: "SFProDisplaySemiBold",
+        }}
+      >
+        Hello World
+      </Text>
+      <IconOpt />
+    </View>
+    <View>
+      <View style={{ position: "absolute", top: 0, right: 0, zIndex: 1 }}>
+        <View
+          style={{
+            width: 11,
+            height: 11,
+            borderRadius: 100,
+            borderWidth: 1.4,
+            borderColor: "#47BBE1",
+            backgroundColor: "#FF7C7C",
+          }}
+        />
+      </View>
+      <IconNotif />
+    </View>
+  </View>
+);
 
 export default function Home() {
-  const rainyGradient = ["#08244F", "#134CB5", "#0B42AB"];
-  const sunnyGradient = ["#29B2DD", "#33AADD", "#2DC8EA"];
+  const colorScheme = useColorScheme();
+  // const colors = colorScheme === "dark" ? darkColors : lightColors;
+  const colors = darkColors;
+  const insets = useSafeAreaInsets();
+
+  const MainContent = () => (
+    <View
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Image
+        source={
+          colorScheme === "dark"
+            ? require("../../assets/images/Dark.png")
+            : require("../../assets/images/Light.png")
+        }
+        style={{ width: 300, height: 200 }}
+      />
+      <Text
+        style={{
+          fontSize: 64,
+          color: "#fff",
+          fontFamily: "SFProDisplaySemiBold",
+          textAlign: "center",
+        }}
+      >
+        30º
+      </Text>
+      <Text
+        style={{
+          fontSize: 18,
+          color: "#fff",
+          fontFamily: "SFProDisplayRegular",
+          textAlign: "center",
+        }}
+      >
+        {`Precipitations\nMax.:34º   Min.:28º`}
+      </Text>
+    </View>
+  );
+
   return (
     <>
       <LinearGradient
-        colors={rainyGradient}
-        style={styles.background}
+        colors={colors.gradient} // change later
+        style={styles(colors).background}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
-        locations={[0, 0.6, 1]}
+        locations={[0, 0.45, 1]}
       />
-      <View style={styles.container}>
-        <Text
-          style={[styles.fontTest, { fontFamily: "AlegreyaSans_500Medium" }]}
-        >
-          AlegreyaSans_500Medium
-        </Text>
-        <Text style={[styles.fontTest, { fontFamily: "AlegreyaSans_700Bold" }]}>
-          AlegreyaSans_700Bold
-        </Text>
-        <Text style={[styles.fontTest, { fontFamily: "SFProDisplayRegular" }]}>
-          SFProDisplayRegular
-        </Text>
-        <Text style={[styles.fontTest, { fontFamily: "SFProDisplaySemiBold" }]}>
-          SFProDisplaySemiBold
-        </Text>
-        <Text style={[styles.fontTest, { fontFamily: "SFProDisplayBold" }]}>
-          SFProDisplayBold
-        </Text>
+      <View
+        style={[
+          styles(colors).container,
+          {
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+          },
+        ]}
+      >
+        <Header />
+        <MainContent />
       </View>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  background: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    height: "100%",
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 10,
-  },
-  fontTest: {
-    fontSize: 24,
-    color: "#fff",
-  },
-});
+const styles = (colors) =>
+  StyleSheet.create({
+    background: {
+      position: "absolute",
+      left: 0,
+      right: 0,
+      top: 0,
+      height: "100%",
+    },
+    container: {
+      flex: 1,
+      // alignItems: "center",
+      // justifyContent: "center",
+      gap: 10,
+    },
+    fontTest: {
+      fontSize: 18,
+      color: "#fff",
+    },
+  });
---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+export const lightColors = {
+  background: "#104084",
+  gradient: ["#29B2DD", "#33AADD", "#2DC8EA"],
+};
+
+export const darkColors = {
+  background: "#001026",
+  gradient: ["#08244F", "#134CB5", "#0B42AB"],
+};
